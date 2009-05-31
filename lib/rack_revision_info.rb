3e39d8f25db07c3fc242706940559cea67613c69
Marcin Kulik <marcin.kulik@gmail.com>
2009-05-31 17:52:16 +0200

    ignoring xhr requests, added specs

--- lib/rack_revision_info.rb
+++ lib/rack_revision_info.rb
@@ -4,6 +4,22 @@ module Rack
     def initialize(app, opts={})
       @app = app
       path = opts[:path] or raise ArgumentError, "You must specify directory of your local repository!"
+      revision, date = get_revision_info(path)
+      @revision_info = "Revision #{revision || 'unknown'}"
+      @revision_info << " (#{date.strftime("%Y-%m-%d %H:%M:%S %Z")})" if date
+    end
+
+    def call(env)
+      status, headers, body = @app.call(env)
+      if headers['Content-Type'] == 'text/html' && !Rack::Request.new(env).xhr?
+        body << "\n" << %(<!-- #{@revision_info} -->)
+      end
+      [status, headers, body]
+    end
+
+    protected
+
+    def get_revision_info(path)
       case detect_type(path)
       when :git
         info = `cd #{path}; LC_ALL=C git log -1 --pretty=medium`
@@ -16,19 +32,8 @@ module Rack
       else
         revision, date = nil, nil
       end
-      @revision_info = "Revision #{revision || 'unknown'}"
-      @revision_info << " (#{date.strftime("%Y-%m-%d %H:%M:%S %Z")})" if date
+      [revision, date]
     end
-
-    def call(env)
-      status, headers, body = @app.call(env)
-      if headers['Content-Type'] == 'text/html'
-        body << "\n" << %(<!-- #{@revision_info} -->)
-      end
-      [status, headers, body]
-    end
-
-    protected
 
     def detect_type(path)
       return :git if ::File.directory?(::File.join(path, ".git"))
---
+++ spec/spec_rack_revision_info.rb
@@ -0,0 +1,53 @@
+require 'spec'
+require 'rack/builder'
+require 'rack/mock'
+require File.join(File.dirname(__FILE__), '..', 'lib', 'rack_revision_info.rb')
+
+class Rack::RevisionInfo
+  def get_revision_info(path)
+    [REV, DATE]
+  end
+end
+
+describe "Rack::RevisionInfo" do
+  REV = "a4jf64jf4hff"
+  DATE = DateTime.now
+
+  it "should append revision info in html comment" do
+    app = Rack::Builder.new do
+      use Rack::RevisionInfo, :path => "/some/path/to/repo"
+      run lambda { |env| [200, { 'Content-Type' => 'text/html' }, ["<html><head></head><body>Hello, World!</body></html>"]] }
+    end
+    response = Rack::MockRequest.new(app).get('/')
+    response.body.should match(/#{Regexp.escape("<!-- Revision #{REV} (#{DATE.strftime("%Y-%m-%d %H:%M:%S %Z")}) -->")}/)
+  end
+
+  it "doesn't append revision info for non-html content-types" do
+    app = Rack::Builder.new do
+      use Rack::RevisionInfo, :path => "/some/path/to/repo"
+      run lambda { |env| [200, { 'Content-Type' => 'text/plain' }, ["Hello, World!"]] }
+    end
+    response = Rack::MockRequest.new(app).get('/')
+    response.body.should_not match(/#{Regexp.escape('<!-- Revision ')}/)
+  end
+
+  it "doesn't append revision info for xhr requests" do
+    app = Rack::Builder.new do
+      use Rack::RevisionInfo, :path => "/some/path/to/repo"
+      run lambda { |env| [200, { 'Content-Type' => 'text/html' }, ["<html><head></head><body>Hello, World!</body></html>"]] }
+    end
+    response = Rack::MockRequest.new(app).get('/', "HTTP_X_REQUESTED_WITH" => "XMLHttpRequest")
+    response.body.should_not match(/#{Regexp.escape('<!-- Revision ')}/)
+  end
+
+  it "raises exeption when no path given" do
+    app = Rack::Builder.new do
+      use Rack::RevisionInfo
+      run lambda { |env| [200, {'Content-Type' => 'text/plain'}, ["Hello, World!"]] }
+    end
+    lambda do
+      response = Rack::MockRequest.new(app).get('/')
+    end.should raise_error(ArgumentError)
+  end
+
+end
