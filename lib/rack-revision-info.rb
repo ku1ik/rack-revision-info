f34bd41159ea7828cb5f125dd7ad6f52d5aedd04
Marcin Kulik <marcin.kulik@gmail.com>
2009-05-31 14:48:24 +0200

    added support for svn repos, added autodetecting of repository type

--- lib/rack-revision-info.rb
+++ lib/rack-revision-info.rb
@@ -3,31 +3,37 @@ module Rack
     
     def initialize(app, opts={})
       @app = app
-      path = opts[:path] or raise ArgumentError, "You must specify directory!"
-      type = opts[:type] || detect_type
-      case type.to_sym
+      path = opts[:path] or raise ArgumentError, "You must specify directory of your local repository!"
+      case detect_type(path)
       when :git
-        info = `cd #{path}; git log -1 --pretty=medium`
-        @revision, @date = info[/commit\s([a-z0-9]+)/, 1], DateTime.parse(info[/Date:\s+(.+)$/, 1])
+        info = `cd #{path}; LC_ALL=C git log -1 --pretty=medium`
+        revision = info[/commit\s([a-z0-9]+)/, 1]
+        date = (d = info[/Date:\s+(.+)$/, 1]) && (DateTime.parse(d) rescue nil)
       when :svn
-        @revision, @date = "", ""
+        info = `cd #{path}; LC_ALL=C svn info`
+        revision = info[/Revision:\s(\d+)$/, 1]
+        date = (d = info[/Last Changed Date:\s([^\(]+)/, 1]) && (DateTime.parse(d.strip) rescue nil)
       else
-        raise ArgumentError, "Unknown repository type '#{type}'"
+        revision, date = nil, nil
       end
+      @revision_info = "Revision #{revision || 'unknown'}"
+      @revision_info << " (#{date.strftime("%Y-%m-%d %H:%M:%S %Z")})" if date
     end
 
     def call(env)
       status, headers, body = @app.call(env)
       if headers['Content-Type'] == 'text/html'
-        body << "\n" << %(<!-- Revision #{@revision} (#{@date}) -->)
+        body << "\n" << %(<!-- #{@revision_info} -->)
       end
       [status, headers, body]
     end
 
     protected
 
-    def detect_type
-      :git
+    def detect_type(path)
+      return :git if ::File.directory?(::File.join(path, ".git"))
+      return :svn if ::File.directory?(::File.join(path, ".svn"))
+      :unknown
     end
 
   end
